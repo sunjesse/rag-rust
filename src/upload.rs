f9147ea504eafc45502fb0191fb5be653d547884
sunjesse <jessera@outlook.com>
2024-01-30 20:31:21 -0500

    fix tabbing

--- src/upload.rs
+++ src/upload.rs
@@ -5,23 +5,23 @@ use serde_json::{json, Value};
 mod embeddings;
 
 struct Row {
-	r1: String,
-	r2: String,
+    r1: String,
+    r2: String,
 }
 
 pub fn read_rows(&str path) -> Result<Vec<Row>>{
-	let mut csv = Reader::from_path(path)?;
-	let mut batch = Vec::new(); 
+    let mut csv = Reader::from_path(path)?;
+    let mut batch = Vec::new(); 
  
-	for r in csv.deserialize(){
-		let record: Row = r?;
-		batch.push(record);	
-	}
-	Ok(batch)
+    for r in csv.deserialize(){
+        let record: Row = r?;
+        batch.push(record); 
+    }
+    Ok(batch)
 }
 
 fn embed_rows(args: Args, batch: Vec<Row>) -> Result<Vec<Pointstruct>{
-	let source = args.to_tokenizer_source();
+    let source = args.to_tokenizer_source();
     let arch = args.model_architecture;
     let path = args.model_path;
     let params = llm::ModelParameters::default();
@@ -36,21 +36,21 @@ fn embed_rows(args: Args, batch: Vec<Row>) -> Result<Vec<Pointstruct>{
         panic!("Failed to load {arch} model from {path:?}: {err}")
     });
     let infer_params = llm::InferenceParameters::default();
-	
-	let embd = batch.iter().map(|r| embeddings::get_embeddings(model.as_ref(), &infer_params, r.r2));
-	let points = Vec::new();
-	let i = 0;
-	for (j, e) in &embd.iter().enumerate() {
-		let payload: Payload = json!(
-			{
-				"rows": {
-					"0": batch[j].r1,
-					"1": batch[j].r2, 
-				}
-			}
-		)
-		let point = PointStruct::new(i, e.clone(), payload); 	
-		points.push(point);
-	}
-	Ok(points);
+    
+    let embd = batch.iter().map(|r| embeddings::get_embeddings(model.as_ref(), &infer_params, r.r2));
+    let points = Vec::new();
+    let i = 0;
+    for (j, e) in &embd.iter().enumerate() {
+        let payload: Payload = json!(
+            {
+                "rows": {
+                    "0": batch[j].r1,
+                    "1": batch[j].r2, 
+                }
+            }
+        )
+        let point = PointStruct::new(i, e.clone(), payload);    
+        points.push(point);
+    }
+    Ok(points);
 }
