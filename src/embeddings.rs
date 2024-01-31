cc8b4565801f29f9ebc81863de01220fbd202298
sunjesse <jessera@outlook.com>
2024-01-31 06:16:34 -0500

    fix tabbing

--- src/embeddings.rs
+++ src/embeddings.rs
@@ -13,14 +13,14 @@ pub fn load(args: &Args) -> Result<(Box<dyn Model>, &str), Box<dyn std::error::E
         params,
         llm::load_progress_callback_stdout,
     )
-	.map_err(|err| {
+    .map_err(|err| {
         Box::new(err) as Box<dyn std::error::Error>
     })?;
     let query = args
         .query
         .as_deref()
         .unwrap_or("This is a default query");
-	Ok((model, query))
+    Ok((model, query))
 } 
 
 pub fn get_embeddings(
@@ -46,8 +46,8 @@ pub fn get_embeddings(
 }
 
 pub fn load_and_embed(args: Args) -> Result<(String, Vec<f32>), Box<dyn std::error::Error>> {
-	let (model, query) = load(&args)?;
+    let (model, query) = load(&args)?;
     let infer_params = llm::InferenceParameters::default();
-	let embeddings = get_embeddings(model.as_ref(), &infer_params, query);
+    let embeddings = get_embeddings(model.as_ref(), &infer_params, query);
     Ok((query.to_string(), embeddings))
 }
